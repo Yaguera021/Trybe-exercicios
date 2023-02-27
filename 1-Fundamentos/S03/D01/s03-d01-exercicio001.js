bb6af112e302046cd9dd9e68450622d185834a35
Seu nome <yago_bohmcruz@hotmail.com>
2023-02-27 17:19:41 -0300

    Primeiros exercícios 1-4 prontos.

---
+++ 1-Fundamentos/S03/D01/s03-d01-exercicio001.js
@@ -0,0 +1,8 @@
+let a = 21;
+let b = 12;
+
+console.log ("Adição de a+b = " + (a+b));
+console.log ("Subtração de a-b = " + (a-b));
+console.log ("Multiplicação de a*b = " + (a*b));
+console.log ("Divisão de a/b = " + (a/b));
+console.log ("Módulo de a%b = " + (a%b));
---
+++ 1-Fundamentos/S03/D01/s03-d01-exercicio002.js
@@ -0,0 +1,8 @@
+let numMaior = 30;
+let numMenor = 70;
+if (numMaior > numMenor) {
+    console.log(numMaior)
+} else {
+    console.log("Redefina os valores e/ou altere os parâmetros de If")
+}
+
