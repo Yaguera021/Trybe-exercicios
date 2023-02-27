2a6bf02474a816dff5c64fec59b8f71d8d4ade73
Seu nome <yago_bohmcruz@hotmail.com>
2023-02-27 19:16:38 -0300

    Exercicio 08 - true or false com apenas um IF

---
+++ 1-Fundamentos/S03/D01/s03-d01-exercicio008.js
@@ -0,0 +1,11 @@
+// Necessário informar os valores para as variáveis a,b e c // 
+
+let a;
+let b;
+let c;
+
+if (a%2 === 0 || b%2 === 0 || c%2 === 0){
+    console.log('True')
+} else {
+    console.log('False');
+}
