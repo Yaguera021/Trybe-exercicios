03e33972792b71a06d55e58ef08f340bc658c16d
Seu nome <yago_bohmcruz@hotmail.com>
2023-02-27 19:21:51 -0300

    Exercicio 09 e correção no exercício 08

--- 1-Fundamentos/S03/D01/s03-d01-exercicio008.js
+++ 1-Fundamentos/S03/D01/s03-d01-exercicio008.js
@@ -1,11 +1,12 @@
 // Necessário informar os valores para as variáveis a,b e c // 
 
-let a;
-let b;
-let c;
+let a = 51;
+let b = 25;
+let c = 30;
+
+let isEven = false;
 
 if (a%2 === 0 || b%2 === 0 || c%2 === 0){
-    console.log('True')
-} else {
-    console.log('False');
+    isEven = true;
 }
+console.log(isEven);
---
+++ 1-Fundamentos/S03/D01/s03-d01-exercicio009.js
@@ -0,0 +1,12 @@
+// Necessário informar os valores para as variáveis a,b e c // 
+
+let a;
+let b;
+let c;
+
+let isOdd = false;
+
+if (a%2 != 0 || b%2 != 0 || c%2 != 0){
+    isOdd = true;
+}
+console.log(isOdd);
