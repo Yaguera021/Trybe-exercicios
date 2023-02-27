ef1c81ada1c5cd2db39d6b961edc152fe3aef590
Seu nome <yago_bohmcruz@hotmail.com>
2023-02-27 19:04:14 -0300

    Exercicio 07 - NOTAS DE A...F

---
+++ 1-Fundamentos/S03/D01/s03-d01-exercicio007.js
@@ -0,0 +1,19 @@
+// O valor a ser inserido em 'nota' deverá ser a porcentagem! //
+
+let nota;
+
+if (nota >= 90) {
+    console.log('NOTA A');
+} else if (nota >= 80) {
+    console.log('NOTA B');
+} else if (nota >= 70) {
+    console.log('NOTA C')
+} else if ( nota >= 60) {
+    console.log('NOTA D') 
+} else if (nota >= 50) {
+    console.log('NOTA E')
+} else if ( nota > 50) {
+    console.log('NOTA F')
+} else {
+    console.log('Reveja o valor inserido a nota!');
+}
