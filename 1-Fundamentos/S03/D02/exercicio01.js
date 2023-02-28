30bd377535e00ff0f535163dec61648ff51d9b96
Seu nome <yago_bohmcruz@hotmail.com>
2023-02-28 17:56:51 -0300

    Identificando maior numero

--- 1-Fundamentos/S03/D02/exercicio01.js
+++ 1-Fundamentos/S03/D02/exercicio01.js
@@ -1,13 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let sum = 0;
+let maiorNumero = numbers[0];
 for ( let index = 0; index < numbers.length; index += 1) {
-    sum += numbers[index];
+    if (numbers[index] > maiorNumero) {
+        maiorNumero = numbers[index]
+    }
+
 }
-let media = sum / numbers.length
-if(media > 20) {
-    console.log('Valor maior que 20');
-} else {
-    console.log('Valor menor que 20');
-}
+ console.log(maiorNumero);
 
 
